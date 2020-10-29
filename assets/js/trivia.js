ff31c6ccd3c16b8a259d12a6b1402f50ee590ae5
Mystic Kitty <djabranton@gmail.com>
2020-10-29 13:42:25 -0700

    More updates.

--- assets/js/trivia.js
+++ assets/js/trivia.js
@@ -1,8 +1,8 @@
 //var highScores = JSON.parse(localStorage.getItem("highScores")) || []
 var displayStatus = document.getElementById("answerStatus")
+var nextButton = document.getElementById("nextQuestion")
 var currentQuestion = 0
 var score = 0
-
 
 const api = {
     base: "https://opentdb.com/api.php",
@@ -38,55 +38,61 @@ var showQuiz = function (trivia) {
     document.getElementById("category").innerHTML = "Category:" + trivia.results[0].category
     document.getElementById("difficulty").innerHTML = "Difficulty:" + trivia.results[0].difficulty
     document.getElementById("question").innerHTML = "Question:" + trivia.results[0].question
-    document.getElementById("answer1").innerHTML = trivia.results[0].correct_answer
-    document.getElementById("answer2").innerHTML = trivia.results[0].incorrect_answers[0]
-    document.getElementById("answer3").innerHTML = trivia.results[0].incorrect_answers[1]
-    document.getElementById("answer4").innerHTML = trivia.results[0].incorrect_answers[2]
-    answserQuiz();
-}
 
-var answserQuiz = function () {
+
+    document.getElementById("answer1").style = "display:inline"
+    document.getElementById("answer1").innerHTML = trivia.results[0].correct_answer
+
+    document.getElementById("answer2").style = "display:inline"
+    document.getElementById("answer2").innerHTML = trivia.results[0].incorrect_answers[0]
+
+    document.getElementById("answer3").style = "display:inline"
+    document.getElementById("answer3").innerHTML = trivia.results[0].incorrect_answers[1]
+
+    document.getElementById("answer4").style = "display:inline"
+    document.getElementById("answer4").innerHTML = trivia.results[0].incorrect_answers[2]
+
+
     var correctButton = document.getElementById("answer1")
-    var nextButton = document.getElementById("nextQuestion")
 
     correctButton.addEventListener("click", function (event) {
         event.preventDefault();
+        document.getElementById("answerStatus").innerHTML = "Way to Go! Yes, the correct answer is " + trivia.results[0].correct_answer
         console.log("correct");
-        score = score + 5
-        document.getElementById("score").innerHTML = "Your score is " + score
-        document.getElementById("answerStatus").innerHTML = "Correct! Way to Go!"
-        document.querySelectorAll(".answer").style = "display:none"
-        quizStatus()
+        var correct = true
+        quizStatus(correct)
     })
 
     document.querySelectorAll(".incorrect").forEach((item) => {
         item.addEventListener("click", (event) => {
             console.log("incorrect");
             event.preventDefault();
-            document.getElementById("answerStatus").innerHTML = "Nope!  That's not correct. The correct answer is " + trivia.results[0].correct_answer
-            document.querySelectorAll(".answer").style = "display:none"
-            console.log("boo!")
+            document.getElementById("answerStatus").innerHTML = "Nope! That's not correct. The correct answer is " + trivia.results[0].correct_answer
             quizStatus()
         });
     });
-
-    nextButton.addEventListener("click", function (event) {
-        event.preventDefault()
-        document.getElementById("nextQuestion").style = "display:none"
-        clearContent()
-        playTrivia()
-    })
 }
 
-var clearContent = function () {
-    displayStatus.innerHTML = ""
-};
+var quizStatus = function (correct) {
 
-function quizStatus() {
+    if (correct) {
+        score = score++
+        document.getElementById("score").innerHTML = "Your score is " + score
+
+
+    } else {
+        document.getElementById("score").innerHTML = "Your score is " + score
+    }
 
     currentQuestion++
-    if (currentQuestion < 9) {
+    if (currentQuestion <= 10) {
+        console.log(currentQuestion)
+        document.getElementById("answer1").style = "display:none"
+        document.getElementById("answer2").style = "display:none"
+        document.getElementById("answer3").style = "display:none"
+        document.getElementById("answer4").style = "display:none"
         document.getElementById("nextQuestion").style = "display:block"
+
     } else {
         console.log("You've finished!")
         document.getElementById("nextQuestion").style = "display:none"
@@ -94,5 +100,16 @@ function quizStatus() {
     }
 };
 
+nextButton.addEventListener("click", function (event) {
+    event.preventDefault()
+    document.getElementById("nextQuestion").style = "display:none"
+    clearContent()
+    playTrivia()
+})
+
+
+var clearContent = function () {
+    displayStatus.innerHTML = ""
+};
 
 playTrivia();

020972de410b85c5a9d7c7a83d883a05d45848b9
Pedal <jmb181@humboldt.edu>
2020-10-31 13:40:05 -0700

    added read and save buttons with local storage

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,11 +1,13 @@
 
+var books = {};
 
 // When the read button is clicked
 $("#read-button").on("click", function() {
     
-    // clear out div contents
+    // clear out div contents, loadbooks, getrandomBook
     $("#content-cell").empty();
     $("#sectionContainer").hide();
+    loadBooks();
     getRandomBook();
 });
 
@@ -21,10 +23,18 @@ var getRandomBook = function(){
         .then(response => response.json())
         .then(data => {
             var bookTitle = data.title;
-            var bookAuthor = data.authors[0].name;
+            var bookAuthor = "";
             var bookSubject = data.subjects[0];
             var bookImage = data.formats["image/jpeg"];
             var bookURL = "";
+            
+            // check if author exists
+            if (data.authors[0].name) {
+                bookAuthor = data.authors[0].name;
+            }
+            else {
+                bookAuthor = "No Author Given";
+            }
             
             // check for different book formats
             if (data.formats["text/html"]){
@@ -44,13 +54,53 @@ var getRandomBook = function(){
             var bookTitleEl = $("<h4>").text(bookTitle);
             var bookAuthorEl = $("<p>").text("Author: " +bookAuthor);
             var bookSubjectEl = $("<p>").text(bookSubject);
-            var bookURLLinkEl = $("<a>").attr({href: bookURL, target: "_blank"}).text("Read Book");
-            var bookURLEl = $("<h4>").append(bookURLLinkEl);
+            
+            // book buttons
+            var readButtonEl = $("<button>").text("Read").attr({id: "readBtn",class:"orange darken-4 z-depth-2 waves-effect waves-light hoverable"}).on("click", function(){
+                window.open(bookURL, "_blank");
+            });
+            var saveButtonEl = $("<button>").text("Save").attr({id: "saveBtn",class:"orange darken-4 z-depth-2 waves-effect waves-light hoverable"}).on("click", function(){
+                saveBook(bookTitle,bookURL);
+            });
+            
+            var bookDivEl = $("<div>").attr("id", "bookBtnDiv").append(readButtonEl,saveButtonEl);
+
             var bookImageEl = $("<img>").attr("src", bookImage);
-            $("#content-cell").append(bookTitleEl,bookAuthorEl,bookSubjectEl, bookURLEl, bookImageEl);
+            
+            // append all elements to content-cell div
+            $("#content-cell").append(bookTitleEl,bookAuthorEl,bookSubjectEl, bookDivEl, bookImageEl);
             
         })
         .catch((error) => {
-            bookTitleEl.text("Error, Please Try Again.");
+            var bookTitleEl = $("<h4>").text("Error, Please Try Again.");
+            $("#content-cell").append(bookTitleEl);
+            console.log(error);
         });
+};
+
+// Save book to localStorage
+var saveBook = function(title,url) {
+    
+    books[title] = url;
+    localStorage.setItem("books", JSON.stringify(books));
+    loadBooks();
+};
+
+// load books from localStorage
+var loadBooks = function() {
+    books = JSON.parse(localStorage.getItem("books"));
+    $("#bookshelf").empty();
+    var bookShelfListEl = $("<ul>");
+    
+    if (!books) {
+        books = {};
+    }
+    
+    $.each(books, function(key,value){
+        console.log(key,value);
+        var bookItemEl = $("<li>").text(key);
+        bookShelfListEl.append(bookItemEl);
+    })
+
+     $("#bookshelf").append(bookShelfListEl);
 };

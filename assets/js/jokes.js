75838bd4a70d539a4aaa20f493f4166230f8a198
Anthony H Kieu <anthonykieu1@Anthonys-MBP.fios-router.home>
2020-11-01 22:44:35 -0500

    modified js to get buttons to work

--- assets/js/jokes.js
+++ assets/js/jokes.js
@@ -1,5 +1,5 @@
 // Fetches jokes from api //
-var grabJoke = function() {
+var grabJoke = function () {
     fetch("https://icanhazdadjoke.com/", {
         headers: {
             "accept": "application/json"
@@ -7,31 +7,27 @@ var grabJoke = function() {
     })
         .then(data => data.json())
         .then(obj => $("#fetchedJoke").text(obj.joke))
-        console.log($("#fetchedJoke".text));
+    console.log($("#fetchedJoke".text));
 };
 
 // Joke activity to make it visible //
-$("#joke-button").click(function () {
+$("#joke-button").on("click", function () {
     $("#content-cell").empty();
-   
     var jokeContainerEL = $("<div>").attr({ id: "jokeContainer", class: "jokeContainer" });
     var sectionContainerEL = $("<section>").attr({ id: "sectionContainer", class: "custom-content" });
-    var jokeTextEl = $("<p>").attr({ id:"fetchedJoke"});
-    var jokeButtonEl = $("<button>").text("Get Joke").attr({ id: "jokeButton", class: "jokeButton orange darken-4 z-depth-2 waves-effect waves-light hoverable" });
-    var closeJokeButtonEl = $("<button>").text("Exit").attr({ id: "closeJokeButton", class: "jokeButton orange darken-4 z-depth-2 waves-effect waves-light hoverable" });
-    // $("#sectionContainer").show();
+    var jokeTextEl = $("<p>").attr({ id: "fetchedJoke" });
+
+    var jokeButtonEl = $("<button>").text("Get Joke").attr({ id: "jokeButton", class: "jokeButton orange darken-4 z-depth-2 waves-effect waves-light hoverable" }).on("click", function () {
+        grabJoke();
+    });
+
+    var closeJokeButtonEl = $("<button>").text("Exit").attr({ id: "closeJokeButton", class: "jokeButton orange darken-4 z-depth-2 waves-effect waves-light hoverable" }).on("click", function () {
+        $("#content-cell").empty();
+    });
+
+    $("#sectionContainer").show();
     $("#content-cell").append(sectionContainerEL);
     sectionContainerEL.append(jokeContainerEL);
     jokeContainerEL.append(jokeTextEl, jokeButtonEl, closeJokeButtonEl);
     grabJoke();
-});
-
-// Go to the next joke //
-$("#jokeButton").on("click", function () {
-    grabJoke();
-});
-
-// Exit the joke section //
-$("#closeJokeButton").on("click", function () {
-    $("#content-cell").empty();
 });

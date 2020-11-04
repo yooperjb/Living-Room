91bca11448926e90347d965ebe7f578c9b371961
Calista Mayer <calista.a.mayer@gmail.com>
2020-11-03 18:15:56 -0800

    style joke buttons

--- assets/js/jokes.js
+++ assets/js/jokes.js
@@ -13,9 +13,11 @@ var grabJoke = function () {
 // Joke activity to make it visible //
 $("#joke-button").on("click", function () {
     $("#content-cell").empty();
-    var jokeContainerEL = $("<div>").attr({ id: "jokeContainer", class: "jokeContainer" });
-    var sectionContainerEL = $("<section>").attr({ id: "sectionContainer", class: "custom-content" });
+    var jokeContainerEl = $("<div>").attr({ id: "jokeContainer", class: "jokeContainer" });
+    var sectionContainerEl = $("<section>").attr({ id: "sectionContainer", class: "custom-content" });
     var jokeTextEl = $("<p>").attr({ id: "fetchedJoke" });
+
+    var jokeButtonContainerEl = $("<div>").attr({ id: "joke-button-container" });
 
     var jokeButtonEl = $("<button>").text("Get Joke").attr({ id: "jokeButton", class: "jokeButton orange darken-4 z-depth-2 waves-effect waves-light hoverable" }).on("click", function () {
         grabJoke();
@@ -26,8 +28,11 @@ $("#joke-button").on("click", function () {
     });
 
     $("#sectionContainer").show();
-    $("#content-cell").append(sectionContainerEL);
-    sectionContainerEL.append(jokeContainerEL);
-    jokeContainerEL.append(jokeTextEl, jokeButtonEl, closeJokeButtonEl);
+    $("#content-cell").append(sectionContainerEl);
+    sectionContainerEl.append(jokeContainerEl);
+    jokeButtonContainerEl.append(jokeButtonEl, closeJokeButtonEl);
+    jokeContainerEl.append(jokeTextEl, jokeButtonContainerEl);
+    
+    // jokeContainerEL.append(jokeTextEl, jokeButtonEl, closeJokeButtonEl);
     grabJoke();
 });
